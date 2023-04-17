6d59f131471c2ed1e95e426cf014dc9d7e5e0dcc
saumyaborwankar <thesaumyaborwankar@gmail.com>
2023-04-17 18:53:54 -0500

    added frontend

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,39 @@ app.listen(process.env.PORT || 9002, () => {
 const userSchema = new mongoose.Schema({
   name: String,
   tasks: [
-    { status: String, title: String, description: String, dueDate: String },
+    { status: Number, title: String, description: String, dueDate: String },
   ],
 });
 const UserTask = mongoose.model("user", userSchema);
+
+// const user1 = new UserTask({
+//   name: "Saumya",
+//   tasks: [
+//     {
+//       title: "Completing level 2 SESL",
+//       description: "lol",
+//       status: 0,
+//       dueDate: "1999-09-09",
+//     },
+//     {
+//       title: "Completing level 2 SESL",
+//       description: "lol",
+//       status: 0,
+//       dueDate: "1999-09-09",
+//     },
+//   ],
+// });
+// user1.save();
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from backend");
+});
+app.post("/addTask", (req, res) => {
+  try {
+    const name = req.body.name;
+    const { title, description, status, dueDate } = req.body.tasks;
+    res.status(200).send({ message: "added task" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "server error" });
+  }
+});
